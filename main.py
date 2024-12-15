863cd12fd7d76b58d808027aa47d59ae4dd016c0
elijahdunbar <eli.dunbar18@gmail.com>
2024-12-15 18:36:57 -0500

    have to finish the report

---
+++ main.py
@@ -0,0 +1,42 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    book_text = get_text(book_path)
+    word_count = get_word_count(book_text)
+    char_count = get_char_count(book_text)
+    print_report(book_path, word_count, char_count)
+
+    
+def get_text(path):
+    with open(path) as f:
+        book_text = f.read()
+        return book_text
+
+
+def get_word_count(text):
+    words = text.split()
+    return len(words)
+
+
+def get_char_count(text):
+    lowercase_text = text.lower()
+    char_dict = {}
+    for char in lowercase_text:
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
+    return char_dict
+
+
+def print_report(path, word_count, char_dict):
+    print(f"--- Begin report of {path} ---")
+    print(f"{word_count} words found in the document")
+    for char in char_dict:
+        if char == "\n":
+            print(f"The '\\n' character was found {char_dict[char]} times")
+        else:
+            print(f"The '{char}' character was found {char_dict[char]} times")
+    print("--- End report ---")
+
+
+main()
