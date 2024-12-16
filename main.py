13db968e87cdb33fad2d9279c3dc8f3874d1c535
elijah <eli.dunbar18@gmail.com>
2024-12-16 15:04:24 -0500

    mission complete

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ def main():
     word_count = get_word_count(book_text)
     char_count = get_char_count(book_text)
     print_report(book_path, word_count, char_count)
-
     
 def get_text(path):
     with open(path) as f:
@@ -30,17 +29,23 @@ def get_char_count(text):
 
 def print_report(path, word_count, char_dict):
     print(f"--- Begin report of {path} ---")
-    print(f"{word_count} words found in the document")
-    for char in char_dict:
-        if char == "\n":
-            print(f"The '\\n' character was found {char_dict[char]} times")
-        else:
-            print(f"The '{char}' character was found {char_dict[char]} times")
+    print(f"{word_count} words found in the document\n")
+    sorted_chars = sort_dict(char_dict)
+    for char_dict in sorted_chars:
+        if char_dict["name"].isalpha():
+            print(f"The '{char_dict["name"]}' character was found {char_dict["num"]} times")
     print("--- End report ---")
 
 
 def sort_dict(dictionary):
-    pass
+    dict_list = dict_to_list(dictionary)
+    dict_list.sort(reverse=True, key=sort_on)
+    return dict_list
+
+# A function that takes a dictionary and returns the value of the "num" key
+# This is how the `.sort()` method knows how to sort the list of dictionaries
+def sort_on(dictionary):
+    return dictionary["num"]
 
 
 def dict_to_list(dictionary):
